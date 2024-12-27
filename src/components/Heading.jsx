e3e0e095c35cf5586d69a671ce49676d2feea638
Splendorg <splendorg@protonmail.com>
2024-12-27 22:21:01 +0300

    benefits section fix

---
+++ src/components/Heading.jsx
@@ -0,0 +1,10 @@
+
+const Heading = ({ className, title }) => {
+    return (
+        <div className={`max-w-[50rem] mx-auto mb-12 lg:mb-20  ${className || ""}`}>
+            {title && <h2 className="h2">{title}</h2>}
+        </div>
+    )
+}
+
+export default Heading
